16030d0f60626d62f199f3a665fd3df3229683bd
Michael De Guzman <mcdeguzman99@gmail.com>
2015-03-13 01:46:55 +0000

    Pair v2 - attempting to implement solution using functional features of Scala

---
+++ State.scala
@@ -0,0 +1,58 @@
+package connectfour
+
+import java.io.FileNotFoundException
+import java.io.PrintWriter
+import java.io.UnsupportedEncodingException
+import scala.beans.BeanProperty
+import State._
+
+class State(@BeanProperty var player: Player, @BeanProperty var board: Board, @BeanProperty var lastMove: Move)
+  extends Comparable[Any] {
+
+  @BeanProperty
+  var children: Array[State] = length0
+
+  @BeanProperty
+  var value: Int = 0
+
+  def initializeChildren() {
+  }
+
+  def writeToFile() {
+    var writer: PrintWriter = new PrintWriter("output.txt", "UTF-8")
+    try {
+      writer.println(this)
+    } catch {
+      case e@(_: FileNotFoundException | _: UnsupportedEncodingException) => e.printStackTrace()
+    } finally {
+      writer.close();
+    }
+  }
+
+  override def toString(): String = {
+    println("State.toString printing")
+    toStringHelper(0, "")
+  }
+
+  override def compareTo(o: Any): Int = ???
+
+  private def toStringHelper(d: Int, ind: String): String = {
+    var str = ind + player + " to play\n"
+    str = str + ind + "Value: " + value + "\n"
+    str = str + board.toString(ind) + "\n"
+    if (children != null && children.length > 0) {
+      str = str + ind + "Children at depth " + (d + 1) + ":\n" + ind +
+        "----------------\n"
+      for (s <- children) {
+        str = str + s.toStringHelper(d + 1, ind + "   ")
+      }
+    }
+    str
+  }
+}
+
+object State {
+
+  val length0 = Array[State]()
+}
+
